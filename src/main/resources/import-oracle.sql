367661efb27236f1f8f5f18480eb4baacac180d7
Gemzon <eineil.eric.c.gemzon@accenture.com>
2019-09-09 23:20:59 +0800

    ddl-auto is now set to none and schema is created thru import files

--- src/main/resources/import-oracle.sql
+++ src/main/resources/import-oracle.sql
@@ -1,3 +1,4 @@
+DROP TABLE mysql_user IF EXISTS;
 DROP SEQUENCE oracle_seq IF EXISTS;
 
 CREATE SEQUENCE oracle_seq
@@ -8,5 +9,6 @@ INCREMENT BY 1
 NOCACHE
 ORDER;
 
+create table oracle_user (id bigint not null, my_address varchar(255) not null, name varchar(255), primary key (id));
 
 insert into oracle_user (id, name, my_address) values (oracle_seq.nextval, 'Eineil-Oracle', 'Taguig');
